5b21cdb59f59487ec88a698b6ace741b305b68d1
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-06-17 21:47:11 +0900

    add: codegen

---
+++ web/codegen.ts
@@ -0,0 +1,17 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  documents: ['src/**/*.tsx'],
+  generates: {
+    './src/lib/gql/': {
+      preset: 'client',
+    },
+  },
+  hooks: {
+    afterAllFileWrite: ['eslint --fix && prettier --write'],
+  },
+  ignoreNoDocuments: true,
+  schema: 'http://localhost:3000/api/graphql',
+}
+
+export default config
