78cf7743bfca05bf364e9e62054b3e07b4c50b25
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-06-18 21:34:42 +0900

    update: AuthForm

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from '@/client/features/auth/components/AuthForm'
 export default function Home() {
   return (
     <div className={'space-y-4'}>
-      <h1 className={'font-semibold text-gray-600'}>Speakify</h1>
+      <h1 className={'text-2xl font-bold'}>Speakify</h1>
       <AuthForm />
     </div>
   )
--- web/src/client/features/auth/components/AuthForm.tsx
+++ web/src/client/features/auth/components/AuthForm.tsx
@@ -1,21 +1,82 @@
 'use client'
 
+import { Button } from '@/client/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/client/components/ui/form'
+import { Input } from '@/client/components/ui/input'
+import { toast } from '@/client/lib/sonner'
 import { env } from '@/shared/lib/env'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+export const formSchema = z.object({
+  email: z.string().email(),
+})
 
 export function AuthForm() {
+  const [isLoading, setIsLoading] = useState(false)
   const supabase = createClientComponentClient()
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: '',
+    },
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setIsLoading(true)
+
+    const result = await supabase.auth.signInWithOtp({
+      email: values.email,
+      options: {
+        emailRedirectTo: `${env.NEXT_PUBLIC_APP_URL}/auth/callback`,
+      },
+    })
+
+    if (result.error) {
+      toast.error('マジックリンクの送信に失敗しました', {
+        description: result.error.message,
+      })
+    } else {
+      toast.error('マジックリンクをメールで送信しました', {
+        description: 'メールを確認してログインしてください',
+      })
+      form.reset()
+    }
+
+    setIsLoading(false)
+  }
+
   return (
-    <Auth
-      appearance={{ theme: ThemeSupa }}
-      providers={[]}
-      redirectTo={`${env.NEXT_PUBLIC_APP_URL}/auth/callback`}
-      showLinks={false}
-      supabaseClient={supabase}
-      view="magic_link"
-    />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+          control={form.control}
+          name="email"
+        />
+        <Button className={'w-full mt-4'} isLoading={isLoading} type="submit">
+          Send Magic Link
+        </Button>
+      </form>
+    </Form>
   )
 }
