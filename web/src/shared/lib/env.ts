c5ba8aeede303086530a45e4fe673e9ad5ab212e
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-06-18 20:53:14 +0900

    add: supabase

--- web/src/shared/lib/env.ts
+++ web/src/shared/lib/env.ts
@@ -1,13 +1,20 @@
+/* eslint-disable perfectionist/sort-objects */
 import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const env = createEnv({
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    GOOGLE_SERVICE_KEY: process.env.GOOGLE_SERVICE_KEY,
+  client: {
+    NEXT_PUBLIC_SUPABASE_URL: z.string(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   },
   server: {
     DATABASE_URL: z.string(),
     GOOGLE_SERVICE_KEY: z.string(),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+    GOOGLE_SERVICE_KEY: process.env.GOOGLE_SERVICE_KEY,
   },
 })
