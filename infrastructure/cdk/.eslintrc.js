4301c19b9e481a792ec12174dd6cb328f024ec4e
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-06-19 20:06:07 +0900

    add: cdk linter,formatter

---
+++ infrastructure/cdk/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:perfectionist/recommended-natural',
+    'prettier',
+  ],
+}
